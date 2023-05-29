61cef4c3ea14e9893d71f1868b1f9455eaab3198
FerdiHS <92571864+FerdiHS@users.noreply.github.com>
2023-05-29 11:43:37 +0700

    Trrying to change to Railway Database

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,22 @@
 const { createPool } = require("mysql");
 
 const pool = createPool({
+    // Database at Railway
+    
+    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    
+    // Database at local
+    /*
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.MYSQL_DB,
+    */
     // connectionLimit: 50,
 });
 

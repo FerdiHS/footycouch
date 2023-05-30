475bca3231a0a057aaed95c2b9ea97296544b952
FerdiHS <92571864+FerdiHS@users.noreply.github.com>
2023-05-30 18:18:28 +0700

    Updating database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,20 @@ const { createPool } = require("mysql");
 
 const pool = createPool({
     // Database at Railway
-    
+
+    port: 6658,
+    host: containers-us-west-73.railway.app,
+    user: root,
+    password: oQu3G9rkOMqaoBepbSBR,
+    database: railway
+
+    /*
     port: process.env.MYSQLPORT,
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    
+    */
     // Database at local
     /*
     port: process.env.DB_PORT,

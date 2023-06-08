968bba04fd27e87635e33e4f150dfe8827f7620c
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-08 23:11:04 +0700

    Create ProfilePage.js

---
+++ frontend/src/pages/ProfilePage.js
@@ -0,0 +1,10 @@
+import HeaderWebAfterLog from "../components/HeaderWebAfterLog.js"
+import Profile from "../components/Profile.js";
+export default function ProfilePage() {
+    return (
+        <div class="App">
+            <HeaderWebAfterLog />
+            <Profile />
+        </div>
+    );
+}

725288acfa315420fdf8c889dfa0eb100d3801eb
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-09 16:14:40 +0700

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,8 @@
 import HeaderWebAfterLog from "../components/HeaderWebAfterLog.js"
-export default function HomePage() {
+export default function HomePage({setToken}) {
     return (
         <div class="App">
-            <HeaderWebAfterLog />
+            <HeaderWebAfterLog setToken={setToken}/>
             <h2> Home </h2>  
         </div>
     );

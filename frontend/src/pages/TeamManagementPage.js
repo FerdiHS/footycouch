afac7e12aa7815097a784bc61146a55f3e30d26f
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-09 16:13:34 +0700

    Update TeamManagementPage.js

--- frontend/src/pages/TeamManagementPage.js
+++ frontend/src/pages/TeamManagementPage.js
@@ -1,6 +1,6 @@
 import HeaderWebAfterLog from "../components/HeaderWebAfterLog.js";
 import TeamManagement from "../components/TeamManagement.js";
-export default function TeamManagementPage() {
+export default function TeamManagementPage({setToken}) {
     return (
         <div>
             <HeaderWebAfterLog setToken={setToken}/>

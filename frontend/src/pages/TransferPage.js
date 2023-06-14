067c8b509b7a792abdd0153bf38abe7966ca7190
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-14 21:27:10 +0700

    Update TransferPage.js

--- frontend/src/pages/TransferPage.js
+++ frontend/src/pages/TransferPage.js
@@ -1,5 +1,5 @@
-import HeaderWebAfterLog from "../component/HeaderWebAfterLog.js"
-import Transfer from "../component/Transfer.js";
+import HeaderWebAfterLog from "../components/HeaderWebAfterLog.js"
+import Transfer from "../components/Transfer.js";
 export default function TransferPage({setToken}) {
     return (
         <div class="App">

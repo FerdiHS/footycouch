d186cf99a95574185b87a6c0be39df324efdd1a6
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-23 23:02:38 +0700

    Update Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -179,12 +179,12 @@ export default function Profile() {
             ]);
             const updatedPlayers = await Promise.all(
                 players.map(async p => {
-                    const playerResp = (await axios.get("https://footycouch-production.up.railway.app/players/id/" + p.id)).data;
-                    if (playerResp === null) {
+                    if (p.id === null) {
                         p.name = "";
                         p.team = "";
                         return p;
                     }
+                    const playerResp = (await axios.get("https://footycouch-production.up.railway.app/players/id/" + p.id)).data;
                     p.name = playerResp.web_name;
                     p.teamId = playerResp.team;
                     const teamResp = (await axios.get("https://footycouch-production.up.railway.app/teams/id/" + p.teamId)).data;

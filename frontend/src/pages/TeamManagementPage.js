32523fd0258804966d21634964adc3ac3be7560a
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-24 17:23:40 +0700

    Update TeamManagementPage.js

--- frontend/src/pages/TeamManagementPage.js
+++ frontend/src/pages/TeamManagementPage.js
@@ -1,10 +1,102 @@
 import HeaderWebAfterLog from "../components/HeaderWebAfterLog.js";
 import TeamManagement from "../components/TeamManagement.js";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import useToken from "./Token";
 export default function TeamManagementPage({setToken}) {
+    var players = [];
+    var formation = "0-0-0";
+    const username = useToken().token;
+    const loadUser = async () => {
+        try {
+            const users = (await axios.get("https://footycouch-production.up.railway.app/users/" + username)).data.data;
+            formation = users.formation;
+            const players = ([
+                {
+                    position: "GKP",
+                    id: users.gk_1,
+                },
+                {
+                    position: "GKP",
+                    id: users.gk_2
+                },
+                {
+                    position: "DEF",
+                    id: users.def_1
+                },
+                {
+                    position: "DEF",
+                    id: users.def_2
+                },
+                {
+                    position: "DEF",
+                    id: users.def_3
+                },
+                {
+                    position: "DEF",
+                    id: users.def_4
+                },
+                {
+                    position: "DEF",
+                    id: users.def_5
+                },
+                {
+                    position: "MID",
+                    id: users.mid_1
+                },
+                {
+                    position: "MID",
+                    id: users.mid_2
+                },
+                {
+                    position: "MID",
+                    id: users.mid_3
+                },
+                {
+                    position: "MID",
+                    id: users.mid_4
+                },
+                {
+                    position: "MID",
+                    id: users.mid_5
+                },
+                {
+                    position: "FWD",
+                    id: users.fow_1
+                },
+                {
+                    position: "FWD",
+                    id: users.fow_2
+                },
+                {
+                    position: "FWD",
+                    id: users.fow_3
+                },
+            ]);
+            const updatedPlayers = await Promise.all(
+                players.map(async p => {
+                    if (p.id === null) {
+                        navigate("/transfer");
+                    }
+                    const playerResp = (await axios.get("https://footycouch-production.up.railway.app/players/id/" + p.id)).data;
+                    p.name = playerResp.web_name;
+                    p.teamId = playerResp.team;
+                    const teamResp = (await axios.get("https://footycouch-production.up.railway.app/teams/id/" + p.teamId)).data;
+                    p.team = teamResp.short_name;
+                    return p;
+                })
+            );
+            players = updatedPlayers;
+            
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    loadUser();
     return (
         <div>
             <HeaderWebAfterLog setToken={setToken}/>
-            <TeamManagement />
+            <TeamManagement passPlayer = {players} passFormation = {formation}/>
         </div>
     );
 }

cfe4cd804b7b9f57c02cbb39870e9b8422177063
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-24 20:55:29 +0700

    Update TeamManagement.js

--- frontend/src/components/TeamManagement.js
+++ frontend/src/components/TeamManagement.js
@@ -11,7 +11,7 @@ export default function TeamManagement({passPlayer, passFormation, passPoint}) {
         "BRE": "Brentford",
         "BOU": "Bournemouth",
         "BHA": "Brighton",
-        "SOU": "Southampton",
+        "SOU": "Southampton",Im
         "CHE": "Chelsea",
         "CRY": "Crystal Palace",
         "FUL": "Fulham",
@@ -24,7 +24,7 @@ export default function TeamManagement({passPlayer, passFormation, passPoint}) {
         "NEW": "Newcastle",
         "TOT": "Tottenham Hotspur",
         "WHU": "Westham",
-        "WOL": "Wolverhampton",
+        "WOL": "Wolferhampton",
         "NFO": "Nottingham Forest"
     }
     const [allformation, setallformation] = useState([

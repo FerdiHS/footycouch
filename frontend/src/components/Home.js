26d7de51d91a3e44441ffa0b18ce3dccbc8c0e91
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-06-25 18:32:55 +0700

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import TextInputPost from "./TextInputPost";
 import useToken from "./Token";
 
-export default function Home({passProfilePicture, passPosts}) {
+export default function Home({passProfilePicture, passPosts, passId}) {
     return (<div class="container6"><div class="post2"><TextInputPost username={useToken().token} posts={passPosts} profilePicture={passProfilePicture}/></div></div>);
 }

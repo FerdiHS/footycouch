05676e44af4a418b028365ca53246a235707722e
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-07-18 23:53:52 +0700

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -46,6 +46,7 @@ export default function Login({setToken}) {
         })
         .then(res => console.log(res))
         .catch(err => {
+            setisLoading(false);
             seterror(true);
             console.log(err);
         });

f7d42fd3a0e10eb744e414d1bef6c058d4466d90
angkyakdifp <123062882+angkyakdifp@users.noreply.github.com>
2023-07-21 21:57:01 +0700

    Add files via upload

---
+++ frontend/src/components/Comment.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ReactLoading from "react-loading";
+
+export default function Comment({comment}) {
+    const [user, setuser] = useState({});
+    const username = user.username;
+    const profilePicture = user.profile_picture;
+    const loadUser = () => {
+        axios.get("https://footycouch-production.up.railway.app/users/id/" + comment.user).then(x => {setuser(x.data.results)})
+    }
+    const navigate = useNavigate();
+    const Enter = str => {
+        for (let i = 0; i < str.length - 1; i++) {
+            if (str.charAt(i) === '\\' && str.charAt(i + 1) === 'n') {
+                return (<>{str.slice(0, i)}<br/>{Enter(str.slice(i + 2))}</>)
+            } 
+        }
+        return str;
+    }
+    if (username === undefined) {
+        loadUser();
+        return (<div style={{position: "relative", top:-30, left:"20%"}}><ReactLoading type="spin" color="#0000FF"
+        height={30} width={15} /></div>)
+    } else {
+        return (
+        <div style={{}}>
+            <strong class="unameComment" onClick={() => navigate('/user/' + username)}>{username}</strong>
+            <div class="profilePicture4"> 
+                <img src={profilePicture} onClick={() => navigate('/user/' + username)}/>
+            </div>
+            <div class="comments">{Enter(comment.content)}</div>
+        </div>)
+    }
+}
